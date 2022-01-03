bb9b21ded049c5774bf91db8f4bde94f759d1e87
antlogist <antlogist@gmail.com>
2022-01-03 20:43:19 +0300

    Routes name added

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home.index');
+
+Route::get('/contact', function () {
+    return 'Contact';
+})->name('home.contact');
