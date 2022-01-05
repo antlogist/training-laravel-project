19a1492d556a27c85c803b4dec441c576118abb4
antlogist <antlogist@gmail.com>
2022-01-05 21:50:44 +0300

    Partials include added

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -19,13 +19,7 @@
   @endif -->
 
   @forelse ($posts as $key => $post)
-    {{-- @break($key == 1) --}}
-   {{-- @continue($key == 1) --}}
-    @if($loop->odd)
-      <h2>{{ $key }} {{ $post['title']}}</h2>
-    @else
-      <h2 style="background-color:lightgray">{{ $key }} {{ $post['title']}}</h2>
-    @endif
+     @include('posts.partials.post')
   @empty
     <h2>No Posts Found</h2>
   @endforelse
