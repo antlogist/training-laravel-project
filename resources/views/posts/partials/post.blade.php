f6b3a83beedfcf334d1eb95ce8f6d85191244c99
antlogist <antlogist@gmail.com>
2022-02-15 21:17:37 +0300

    Blog post deleting implemented

--- resources/views/posts/partials/post.blade.php
+++ resources/views/posts/partials/post.blade.php
@@ -1,5 +1,14 @@
 @if($loop->odd)
-  <h2>{{ $key }} {{ $post['title']}}</h2>
+  <h2>{{ $key }} {{ $post->title}}</h2>
 @else
-  <h2 style="background-color:lightgray">{{ $key }} {{ $post['title']}}</h2>
+  <h2 style="background-color:lightgray">{{ $key }} {{ $post->title}}</h2>
 @endif
+
+<div>
+  <form action="{{ route('posts.destroy', ['post' => $post->id]) }}" method="POST">
+    @csrf
+    @method('DELETE')
+    <input type="submit" value="Delete!">
+
+  </form>
+</div>
