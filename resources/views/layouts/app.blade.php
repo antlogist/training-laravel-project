b8917f6ea19b4f8be8690d8f9d7e435fa26ca169
antlogist <antlogist@gmail.com>
2022-02-15 22:49:32 +0300

    navigation styles added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,17 @@
   <script src="{{ mix('js/app.js') }}"></script>
 </head>
 <body>
-  <div>
+  <div class="d-flex dlex-column flex-md-row align-items-center p-3 px-md-4
+  bg-white border-bottom border-shaddow-sm mb-3">
+    <h5 class="my-0 mr-md-auto font-weight-normal">Laravel App</h5>
+    <nav class="my-2 my-md-0 mr-md-3">
+      <a class="p-2 text-dark" href="{{ route('home.index') }}">Home</a>
+      <a class="p-2 text-dark" href="{{ route('posts.index') }}">Blog Posts</a>
+      <a class="p-2 text-dark" href="{{ route('posts.create') }}">Add Blog Post</a>
+      <a class="p-2 text-dark" href="{{ route('home.contact') }}">Contact</a>
+    </nav>
+  </div>
+  <div class="container">
     @if(session('status'))
     <div style="background: red; color: white;">
       {{ session('status') }}
