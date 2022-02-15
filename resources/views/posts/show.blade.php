d3999aa3d2bfd45a805ff31a73b6c24bd8a8d5a6
antlogist <antlogist@gmail.com>
2022-02-15 23:18:37 +0300

    Styles fixed

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,24 +1,15 @@
 @extends('layouts.app')
 
-@section('title', $post['title'])
+@section('title', $post->title)
 
 @section('content')
 
-@if($post['is_new'])
-<div>A new blog post</div>
-@elseif(!$post['is_new'])
-<div>An old blog post</div>
-@endif
+  <h1>{{ $post->title }}</h1>
+  <p>{{ $post->content }}</p>
+  <p>Added {{ $post->created_at->diffForHumans() }}</p>
 
-@unless($post['is_new'])
-<div>Old post. Unless used</div>
-@endunless
-
-  <h1>{{ $post['title'] }}</h1>
-  <p>{{ $post['content'] }}</p>
-
-  @isset($post['has_comments'])
-  <div>The post has some comments</div>
-  @endisset
+  @if(now()->diffInMinutes($post->created_at) < 5)
+  <div class="alert alert-info">New!</div>
+  @endif
 
 @endsection
